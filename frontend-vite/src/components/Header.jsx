320c5b7f6d990b23c5db1cc50f1a1bd08f8c76d8
Jocke T. Sjölin <jocke.t.sjolin@Jockes-Mac-mini.local>
2026-04-10 09:48:13 +0200

    login form layout

---
+++ frontend-vite/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="border-b">
+      {/* navbar */}
+      Header J
+    </header>
+  );
+};
+
+export default Header;
--- frontend-vite/src/components/Login.jsx
+++ frontend-vite/src/components/Login.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { apiFetch } from "../api/helper";
+import Header from "./Header";
 import { Link } from "react-router-dom";
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 
 export default function Login({ onLogin }) {
   const [email, setEmail] = useState("");
@@ -29,17 +32,35 @@ export default function Login({ onLogin }) {
   }
 
   return (
-    <main className="content">
-      <h1 className="text-4xl">Login</h1>
+    <>
+      <Header />
 
-      <form onSubmit={handleSubmit}>
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
-        <Button type="submit">OK</Button>
-      </form>
+      <main className="mx-auto max-w-4xl px-4 space-y-8 py-12">
+        <h2 className="text-3xl font-semibold text-center m-0">
+          Login
+        </h2>
 
-      <p>No account? Sign up <Link to="/register">here</Link>.</p>
-    </main>
+        <form onSubmit={handleSubmit} className="grid gap-6 max-w-md w-full mx-auto rounded-lg p-6 shadow-sm">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="john.doe@example.com" />
+          </div>
+
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="********" />
+          </div>
+
+          <Button type="submit" className="w-full transition-colors hover:bg-primary/90">Login</Button>
+        </form>
+
+        <p className="text-center text-sm text-muted-foreground">No account? Sign up <Link to="/register" className="text-blue-600 underline">here</Link>.</p>
+      </main>
+
+      <footer className="border-b">
+        Footer
+      </footer>
+    </>
   );
 }
 
