5496fb210352545e06f4aad41e94c70325cc020c
Jocke T. Sjölin <jocke.t.sjolin@Jockes-Mac-mini.local>
2026-05-09 17:02:19 +0200

    input field padding

--- frontend-vite/src/components/TodoItem.jsx
+++ frontend-vite/src/components/TodoItem.jsx
@@ -45,7 +45,7 @@ const TodoItem = ({ level, item, onSave, onAddSiblingItem, onAddSubItem, onDelet
           onBlur={handleBlur}
           onKeyDown={(e) => e.key === "Enter" && handleBlur()}
           autoFocus
-          style={{ fontSize: `${fontSize}px`, padding: `${fontSize}px` }}
+          style={{ fontSize: `${fontSize}px`, padding: `${fontSize*0.5}px` }}
         />
       ) : ( // Show text span if item is not being edited
         <span 
